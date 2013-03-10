9141f641375e257b8eacf8cd22a612a46976654b
maraled2020 <farangis@farangis-Inspiron-1501.(none)>
2013-03-10 21:03:38 +0000

    updated the background.js and window.html

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,6 +1,8 @@
 chrome.app.runtime.onLaunched.addListener(function () {
 	chrome.app.window.create('../window.html', {
 		'width' : 400,
-		'height' : 500
+		'height' : 500,
+		'top' : 100,
+		'left' : 200
 	});
 }); 
